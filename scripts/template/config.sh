19282c2225984e8a8924e508dafa2cbfcd1a7d3b
William Baleson <william.baleson@prometheanworld.com>
2015-09-15 00:31:03 -0400

    leftover files

---
+++ scripts/template/config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+GAME=t
+PORT=8883
+ERA=2
+MASTERPASS=MASTERPASSWORD
+EVENTRARITY=1
+MAGICSITES=50
+MAPMADE=1
+GAMEDIR=/home/steam/dom/scripts/$GAME
+DIR=/home/steam/dominions4/savedgames/$GAME
+HOURS=26
+PROVINCES=180
+THRONES='--thrones 8 0 0 --requiredap 5'
