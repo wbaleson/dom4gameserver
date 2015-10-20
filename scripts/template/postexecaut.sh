b147c154faaeec107c8d7ed9289ec5814818c1bf
will <will@artificers.net>
2015-10-20 18:34:30 +0000

    turn tracker

--- scripts/template/postexecaut.sh
+++ scripts/template/postexecaut.sh
@@ -1,6 +1,6 @@
 cd $1
-/home/steam/scripts/TurnTracker/turntrack.py
-/home/steam/scripts/TurnTracker/makettweb.py
+/home/steam/dom4gameserver/scripts/TurnTracker/turntrack.py
+/home/steam/dom4gameserver/scripts/TurnTracker/makettweb.py
 cp TurnT*.html /home/steam/dom4gameserver/node/dom4gs/client/turntracker
-/home/steam/scripts/TurnTracker/ttindex.py
+/home/steam/dom4gameserver/scripts/TurnTracker/ttindex.py
 
